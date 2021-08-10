fd82ad934e79364d7b44a679be71615a33e82c2e
yahya <crazy20046@gmail.com>
2021-08-10 13:40:07 +0200

    Re arranged the buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,18 +8,18 @@ export default class Calculator extends Component {
       <div className="calculator-container">
         <input className="answer" disabled="disabled" value="0"></input>
         <div className="numbers-container">
-          <Button Addclass="primary" buttonName="AC" />
           <Button Addclass="primary" buttonName="+/-" />
           <Button Addclass="primary" buttonName="%" />
-          <Button Addclass="primary" buttonName="9" />
-          <Button Addclass="primary" buttonName="8" />
+          <Button Addclass="primary" buttonName="AC" />
           <Button Addclass="primary" buttonName="7" />
-          <Button Addclass="primary" buttonName="6" />
-          <Button Addclass="primary" buttonName="5" />
+          <Button Addclass="primary" buttonName="8" />
+          <Button Addclass="primary" buttonName="9" />
           <Button Addclass="primary" buttonName="4" />
-          <Button Addclass="primary" buttonName="3" />
-          <Button Addclass="primary" buttonName="2" />
+          <Button Addclass="primary" buttonName="5" />
+          <Button Addclass="primary" buttonName="6" />
           <Button Addclass="primary" buttonName="1" />
+          <Button Addclass="primary" buttonName="2" />
+          <Button Addclass="primary" buttonName="3" />
           <Button Addclass="primary zero" buttonName="0" />
           <Button Addclass="primary" buttonName="." />
           <div className="secondary-div">
